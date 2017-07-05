b28ad87b93b1b915af703717fae55aa1e36fce23
tomex <tmx@tmx.tokyo>
2017-07-05 21:16:21 +0900

    commit.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ﻿# 5000choyen
 5000兆円風の文字を描画するWebツールです
+f.txtはNoto Sans CJK JP(Heavy)をbase64エンコードしたものです。
--- docs/main.js
+++ docs/main.js
@@ -1,105 +1,69 @@
-var canvas, ctx , textbox;
+var textbox, classText, svg, draw, canvasDraw, iosImg;
 
 window.onload = function () {
-  canvas = document.getElementById("canvas");
-  textbox = document.getElementById("textbox");
-  ctx = canvas.getContext('2d');
-  ctx.font = '100px notobk-subset';
-  ctx.lineJoin = 'round';
+	textbox = document.getElementById("textbox");
+	draw = document.getElementById("draw");
+	svg = document.getElementById("svg");
+	canvasDraw = document.getElementById("canvas-draw");
+	classText = document.querySelectorAll(".text");
+	iosImg = document.getElementById("iosImg");
+	var ua = window.navigator.userAgent.toLowerCase();
+	if ( ua.indexOf('iphone') >= 0 || ua.indexOf('ipod') >= 0 || ua.indexOf('ipad') >= 0 ){
+		document.getElementById("svgView").setAttribute("class","hidden");
+	}else{
+		iosImg = undefined;
+		document.getElementById("iosView").setAttribute("class","hidden");
+	}
+	redraw();
 };
 
 function saveImage() {
-  var a = document.createElement("a");
-  a.href = canvas.toDataURL("image/png");
-  a.setAttribute("download", "image.png");
-  document.body.appendChild(a);
-  a.click();
-  document.body.removeChild(a);
+	var width = svg.getAttribute("width") * 3;
+	var height = svg.getAttribute("height") * 3;
+	var img = new Image;
+	var DOMURL = self.URL || self.webkitURL || self;
+	var blob = new Blob([svg.outerHTML], {type: "image/svg+xml"});
+	var url = DOMURL.createObjectURL(blob);
+	img.onload = function() {
+		var canvas = document.createElement("canvas");
+		canvas.width = width;
+		canvas.height = height;
+		
+		canvasDraw.appendChild(canvas);
+		context = canvas.getContext("2d");
+		context.drawImage(img,0,0,width,height);
+		var name = textbox.value+".png";
+		var data = canvas.toDataURL("image/png");
+			console.log(data);
+		if (iosImg !== undefined){
+			iosImg.setAttribute("src",data);
+			iosImg.setAttribute("alt",name);
+		}else{
+			var a = document.createElement("a");
+			a.setAttribute("download", name);
+			a.href = data;
+			a.click();
+		}
+		DOMURL.revokeObjectURL(url);
+		while (canvasDraw.firstChild) {
+			canvasDraw.removeChild(canvasDraw.firstChild);
+		}
+	}
+	img.src = url;
 }
 
 function redraw() {
-  ctx.setTransform(1,0,0,1,0,0);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.setTransform(1,0,-0.4,1,0,0);
-  var posx = 70;
-  var posy = 100;
-  var text = textbox.value;
-     //黒色
-    {
-      ctx.strokeStyle = "#000";
-      ctx.lineWidth = 22;
-      ctx.strokeText(text, posx + 4, posy + 4);
-    }
-
-  //銀色
-  {
-    var grad = ctx.createLinearGradient(0, 24, 0, 122);
-    grad.addColorStop(0.0, 'rgb(0,15,36)');
-    grad.addColorStop(0.10, 'rgb(255,255,255)');
-    grad.addColorStop(0.18, 'rgb(55,58,59)');
-    grad.addColorStop(0.25, 'rgb(55,58,59)');
-    grad.addColorStop(0.5, 'rgb(200,200,200)');
-    grad.addColorStop(0.75, 'rgb(55,58,59)');
-    grad.addColorStop(0.85, 'rgb(25,20,31)');
-    grad.addColorStop(0.91, 'rgb(240,240,240)');
-    grad.addColorStop(0.95, 'rgb(166,175,194)');
-    grad.addColorStop(1, 'rgb(50,50,50)');
-    ctx.strokeStyle = grad;
-    ctx.lineWidth = 20;
-    ctx.strokeText(text, posx + 4, posy + 4);
-  }
-
-  //黒色
-  {
-    ctx.strokeStyle = "#000000";
-    ctx.lineWidth = 16;
-    ctx.strokeText(text, posx, posy);
-  }
-
-  //金色
-  {
-    var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0, 'rgb(253,241,0)');
-    grad.addColorStop(0.25, 'rgb(245,253,187)');
-    grad.addColorStop(0.4, 'rgb(255,255,255)');
-    grad.addColorStop(0.75, 'rgb(253,219,9)');
-    grad.addColorStop(0.9, 'rgb(127,53,0)');
-    grad.addColorStop(1, 'rgb(243,196,11)');
-    ctx.strokeStyle = grad;
-    ctx.lineWidth = 10;
-    ctx.strokeText(text, posx, posy);
-  }
-
-  //黒
-  ctx.lineWidth = 6;
-  ctx.strokeStyle = '#000';
-  ctx.strokeText(text, posx+2, posy - 3);
-
-  //白
-  ctx.lineWidth = 6;
-  ctx.strokeStyle = '#FFFFFF';
-  ctx.strokeText(text, posx, posy - 3);
-
-  //赤
-  {
-    var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0, 'rgb(255, 100, 0)');
-    grad.addColorStop(0.5, 'rgb(123, 0, 0)');
-    grad.addColorStop(0.51, 'rgb(240, 0, 0)');
-    grad.addColorStop(1, 'rgb(5, 0, 0)');
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = grad;
-    ctx.strokeText(text, posx, posy - 3);
-  }
-
-  //赤
-  {
-    var grad = ctx.createLinearGradient(0, 20, 0, 100);
-    grad.addColorStop(0, 'rgb(230, 0, 0)');
-    grad.addColorStop(0.5, 'rgb(123, 0, 0)');
-    grad.addColorStop(0.51, 'rgb(240, 0, 0)');
-    grad.addColorStop(1, 'rgb(5, 0, 0)');
-    ctx.fillStyle = grad;
-    ctx.fillText(text, posx, posy - 3);
-  }
+	var text = textbox.value;
+	if (classText.length > 0){
+		for(var i=0;i<classText.length;i++){
+			classText[i].innerHTML = text;
+		}
+	}
+	var size = draw.getBBox();
+	var width = size.width + size.x;
+	svg.setAttribute("width",width);
+	if (iosImg !== undefined){
+		iosImg.setAttribute("width",width);
+		saveImage();
+	}
 }
